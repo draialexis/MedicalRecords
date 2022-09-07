4a1a7307277ced7981104ae197f34775d57eed08
Alexis DRAI <aldrai@iutclinfa1015l.iut.local>
2022-09-07 16:49:26 +0200

    Add constructor :smh:

--- src/medicalrecords/Patient.java
+++ src/medicalrecords/Patient.java
@@ -9,5 +9,23 @@ package medicalrecords;
  * @author aldrai
  */
 public class Patient {
+    private String lastName;
+    private String firstName;
+
+    public Patient(String lastName, String firstName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
     
+    public String getLastName(){
+        return this.lastName;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
 }
