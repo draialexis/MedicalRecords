c58bebb425ed932defc0b3886dc47ffb88e93ce0
Alexis DRAI <drai.alexis@gmail.com>
2022-09-07 21:51:52 +0200

    Remove superfluous comments

--- src/medicalrecords/Displayer.java
+++ src/medicalrecords/Displayer.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package medicalrecords;
 
 import java.util.List;
 import java.util.Scanner;
 
-/**
- *
- * @author aldrai
- */
 public class Displayer {
 
     private final PatientManager patientManager;
