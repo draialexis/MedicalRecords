e77df4760177461d96fe414f270700205ffe35e9
Alexis DRAI <aldrai@iutclinfa1015l.iut.local>
2022-09-07 16:54:09 +0200

    Populate PatientManager class

--- src/medicalrecords/PatientManager.java
+++ src/medicalrecords/PatientManager.java
@@ -4,10 +4,28 @@
  */
 package medicalrecords;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author aldrai
  */
 public class PatientManager {
+    private final List<Patient> patients;
+
+    public PatientManager() {
+        this.patients = new LinkedList<>();
+    }
     
+    
+    
+    public List<Patient> getAll(){
+        //TODO make it readonly
+        return this.patients;
+    }
+    
+    public void CreateOne(Patient newPatient){
+        this.patients.add(newPatient);
+    }
 }
